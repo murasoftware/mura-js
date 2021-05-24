f919b28a1151d924aed707e39d7a19bc990d4d9c
Scott Widmer <info@scottwidmer.com>
2021-05-24 15:09:04 -0400

    updating logic

--- packages/mura-next-modules-bs4/src/ItemDate.js
+++ packages/mura-next-modules-bs4/src/ItemDate.js
@@ -3,18 +3,19 @@ import React from 'react';
 export const ItemDate = (props) => {
     let date = '';
     let formatteddate = '';
-  
-    if(props.releasedate){
-      date = new Date(props.releasedate);
-    } else {
-      date = new Date(props.lastupdate);
-    }
-    //console.log(date);
     
-    formatteddate = Intl.DateTimeFormat("en-US", {year: "numeric",month: "long",day: "2-digit"}).format(date);
+    if((props.releasedate != null && props.releasedate != '') || (props.lastupdate != null && props.lastupdate != '')) {
+      if(props.releasedate){
+        date = new Date(props.releasedate);
+      } else {
+        date = new Date(props.lastupdate);
+      }
+      formatteddate = Intl.DateTimeFormat("en-US", {year: "numeric",month: "long",day: "2-digit"}).format(date);
+      return formatteddate
+    }    
 
     return (
-        formatteddate
+        null
     )
   }
 
