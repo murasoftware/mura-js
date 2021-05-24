9286ee3cf67a92cd6e1697decfe7431a41f28343
Scott Widmer <info@scottwidmer.com>
2021-05-24 15:14:01 -0400

    adding featured categories, update from heap

--- packages/mura-next-modules-bs4/src/ArticleMeta.js
+++ packages/mura-next-modules-bs4/src/ArticleMeta.js
@@ -4,46 +4,65 @@ import ItemCredits from './ItemCredits';
 import ItemTags from './ItemTags';
 import OutputMarkup from './OutputMarkup';
 
-const ArticleMeta = (props) => {
-    // console.log('fields ArticleMeta: ' + props.fields);
+export const ArticleMeta = (props) => {
+    console.log('fields ArticleMeta: ' + props.fields);
     
-    const fields = props.fields ? props.fields : 'Date,Credits,Tags';
+    const fields = props.fields ? props.fields : '';
     const fieldlist = fields ? fields.toLowerCase().split(",") : [];
+    const titleclass = props.titleclass ? props.titleclass : '';    
     const item = props.content;
+    let catAssignments = item.categoryassignments;
+    let FeaturedCategoryOnly = "no";
+    if (props.featuredcategoryonly){
+        FeaturedCategoryOnly = "yes";
+    }
+    // console.log('fieldlist: ' + fieldlist);
     return (
-        <div className="pb-4">
+        <div className="mura-article-meta">
             {
                 fieldlist.map(field => {
                     switch(field) {
+                    case "category":
+                        return (
+                            <div key={field} className="mura-item-meta__category">
+                                <ItemCategories categories={catAssignments} featuredonly={FeaturedCategoryOnly} />
+                            </div>
+                        );
                     case "title":
                         return (
-                            <h1 key="title" key={field}>{item.title}</h1>
+                            <h1 key="title" key={field} className={titleclass}>{item.title}</h1>
+                        );
+                    case "menutitle":
+                        return (
+                            <p key="menutitle" key={field} className="mura-item-meta__menutitle">{item.menutitle}</p>
                         );
                     case "summary":
                         return (
-                            <OutputMarkup source={item.summary} className="lead"key={field} />
+                            <OutputMarkup source={item.summary} className="lead" key={field} />
                         );
                     case "date":
                     case "releasedate":
                         return (
-                            <div className="mura-item-meta__date" key="date" key={field}>
-                                <span>Published on: </span> <ItemDate releasedate={item.releasedate} lastupdate={item.lastupdate}></ItemDate>
-                            </div>
+                            <span className="mura-item-meta__date" key={field}>
+                               <ItemDate releasedate={item.releasedate} lastupdate={item.lastupdate}></ItemDate>
+                            </span>
                         );
                     case "credits":
                         if(item.credits){
                             return (
-                                <ItemCredits credits={item.credits} key="credits" key={field} />
+                                <ItemCredits credits={item.credits} key={field} />
                             );
                         }
+                        return null
                     case "tags":
                         if(item.tags){
                             return (
-                                <div className="mura-item-meta__tags" key="tags" key={field}>
-                                    <span>Tags: </span><ItemTags tags={item.tags} key="tags" />
+                                <div className="mura-item-meta__tags" key={field}>
+                                    <ItemTags tagshref="/blog" tags={item.tags} />
                                 </div>
                             );
-                        }                        
+                        }
+                        return null
                     default:
                         return <div className={`mura-item-meta__${field}`} key={field} data-value={props.content[field]}>{props.content[field]}</div>
                     }
