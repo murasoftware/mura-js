6172bfc0c801053a35a420725d8216f1d0980c6e
Scott Widmer <info@scottwidmer.com>
2021-07-21 11:16:18 -0400

    adding variant prop

--- packages/mura-next-modules-bs4/src/ItemTags.js
+++ packages/mura-next-modules-bs4/src/ItemTags.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import Badge from 'react-bootstrap/Badge';
+import { PrimaryNav } from '../dist';
 
 function ItemTags(props){
     
     const Tags = props.tags.split(',');;
     let tagList = [];
     let tag = '';
+    const variant = props.variant ? props.variant : primary;
     // console.log(props.tags);
     for(let i = 0;i < Tags.length;i++) {
         tag = Tags[i];
         tagList.push(
-            <Badge variant="primary mr-2" key={tag}>{tag}</Badge>
+            <Badge variant={`${variant} mr-2`} key={tag}>{tag}</Badge>
         )
     }
 
