f123f5963e26f54a7c17eac16cdf3a6f86e14f6a
Matt Levine <matthewklevine@gmail.com>
2022-12-07 14:24:34 -0800

    Adding to test if the content is html not markdown

--- packages/mura-next-modules-bs4/src/OutputMarkup.js
+++ packages/mura-next-modules-bs4/src/OutputMarkup.js
@@ -99,7 +99,7 @@ function parseStringAsTemplate(stringValue){
 function OutputMarkup({source,className}){
     const parsedSource=parseStringAsTemplate(source);
   
-    if(typeof parsedSource == 'string' && parsedSource && (!parsedSource.startsWith('<') || parsedSource.startsWith('<span') || parsedSource.startsWith('<br'))){
+    if(getMuraConfig().ConnectorConfig.htmleditortype == 'markdown'){
         return(
             <ReactMarkdown plugins={[gfm,slug,directive,htmlDirectives]} allowDangerousHtml renderers={renderers} children={parsedSource} className={className} />
         )
